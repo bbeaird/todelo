766c7d2e58d8667800f177979ead96a60f413679
Brantley Beaird <bbeaird@gmail.com>
2016-10-09 17:06:26 -0700

    Added formula for updating todo ratings.

--- todelo.rb
+++ todelo.rb
@@ -1,7 +1,7 @@
 require "csv"
 
 class Todo
-	attr_accessor :rating
+	attr_accessor :rating, :contents
 
 	# allows user to set an initial rating of the todo
 	# eventually we could bucket them into 3 quantiles
@@ -21,7 +21,7 @@ if File.exist?("todos.csv")
 	p "todos.csv already exists, so we'll be adding todos to that."
 else
 	todos_csv = CSV.open("todos.csv", "wb", headers: true, return_headers: false) do |csv|
-	  csv << ["contents", "rating"]
+		csv << ["contents", "rating"]
 	end
 end
 
@@ -39,24 +39,82 @@ if option == 1
 	end
 end
 
+def return_two_todos
+	todos = []
+	CSV.foreach("todos.csv") do |row|
+		todos << Todo.new(contents: row[0], rating: row[1])
+	end
+	random_index1 = Random.new.rand(1..(todos.length-1))
+	random_index2 = Random.new.rand(1..(todos.length-1))
+	while random_index1 == random_index2
+		random_index2 = Random.new.rand(1..(todos.length-1))
+	end
+	return [todos[random_index1], todos[random_index2]]
+end
+
+def compare(todo1, todo2)
+	p "Which of the following is more important?"
+	p "1: #{todo1.contents}"
+	p "2: #{todo2.contents}"
+	response = gets.chomp.to_i
+	k_factor = 25
+	if response == 1
+		winner_expected = 1.0 / (1.0 + (10 ** ((todo2.rating.to_f - todo1.rating.to_f) / 400.0)))
+		winner_change = k_factor.to_f * (1.0 - winner_expected.to_f)
+		p todo1.rating = (todo1.rating.to_i + winner_change).to_i
+
+		loser_expected = 1.0 / (1.0 + (10 ** ((todo1.rating.to_f - todo2.rating.to_f) / 400.0)))
+		loser_change = k_factor.to_f * (1.0 - loser_expected.to_f)
+		p todo2.rating = (todo2.rating.to_i - loser_change).to_i
+	else
+		winner_expected = 1.0 / (1.0 + (10 ** ((todo1.rating.to_f - todo2.rating.to_f) / 400.0)))
+		winner_change = k_factor.to_f * (1.0 - winner_expected.to_f)
+		p todo1.rating = (todo2.rating.to_i + winner_change).to_i
+
+		loser_expected = 1.0 / (1.0 + (10 ** ((todo2.rating.to_f - todo1.rating.to_f) / 400.0)))
+		loser_change = k_factor.to_f * (1.0 - loser_expected.to_f)
+		p todo1.rating = (todo1.rating.to_i - loser_change).to_i
+	end
+end
+
 if option == 2
 	# start ranking todos
-	todos = []
 	while true
-		CSV.foreach("todos.csv") do |row|
-			todos << row
-		end
-
-		p todos[1..Random.new.rand(todos.length)-1]
-		p todos[1..Random.new.rand(todos.length)-1]
+		p todos_to_compare = return_two_todos
+		compare(todos_to_compare[0], todos_to_compare[1])
 		break
 	end
 end
+
+def match(winner, loser)
+	k_factor = 25
+	winner_expected = 1.0 / (1.0 + (10 ** ((loser[:rating].to_f - winner[:rating].to_f) / 400.0)))
+	p winner_change = k_factor.to_f * (1.0 - winner_expected.to_f)
+	winner[:rating] = (winner[:rating] + winner_change).to_i
+end
+
+
+
+
+
+
 
 p 'Here are the contents and ratings of your current todos...'
 CSV.foreach("todos.csv") do |row|
 	p row
 end
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
