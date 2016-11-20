8e7097f7a6ac404e224d2eb775ddf93e98df3583
Brantley Beaird <bbeaird@gmail.com>
2016-11-20 11:12:33 -0800

    Older stuff...rating works I think.

--- todelo.rb
+++ todelo.rb
@@ -44,6 +44,9 @@ def compare(todo1, todo2)
 		loser_expected = 1.0 / (1.0 + (10 ** ((todo1.rating.to_f - todo2.rating.to_f) / 400.0)))
 		loser_change = k_factor.to_f * (1.0 - loser_expected.to_f)
 		p todo2.rating = (todo2.rating.to_i - loser_change).to_i
+		$todos[todo2.contents] = todo2.rating
+		$todos[todo1.contents] = todo1.rating
+		save_todos
 	else
 		winner_expected = 1.0 / (1.0 + (10 ** ((todo1.rating.to_f - todo2.rating.to_f) / 400.0)))
 		winner_change = k_factor.to_f * (1.0 - winner_expected.to_f)
@@ -51,53 +54,56 @@ def compare(todo1, todo2)
 
 		loser_expected = 1.0 / (1.0 + (10 ** ((todo2.rating.to_f - todo1.rating.to_f) / 400.0)))
 		loser_change = k_factor.to_f * (1.0 - loser_expected.to_f)
-		p todo1.rating = (todo1.rating.to_i - loser_change).to_i
+		p todo2.rating = (todo1.rating.to_i - loser_change).to_i
+		$todos[todo2.contents] = todo2.rating
+		$todos[todo1.contents] = todo1.rating
+		save_todos
 	end
 end
 
 def save_todos
 	todos_csv = CSV.open("todos.csv", "wb", headers: true, return_headers: false) do |csv|
 		# csv << ["contents", "rating"]
-		p "inside save_todos - $todos - #{$todos}" 
-		$todos.each do |todo|
-			csv << [todo.contents, todo.rating]
+		# p "inside save_todos - $todos - #{$todos}" 
+		$todos.each do |content, rating|
+			csv << [content, rating]
 		end
 	end	
 end
 
 def create_or_update_todos
 	# ask user to create new todo or rate current todos
-	p "Push 1 for create new todo; push 2 to rate current todos."
+	p "Push 1 for create new todo; push 2 to rate current todos; push 3 to see list of todos."
 	option = gets.chomp.to_i
 
 	if option == 1
 		p "What do is your todo?"
-		p todo_contents = gets.chomp
+		todo_contents = gets.chomp
 		# later add an option for how important it is; maybe 1, 2, or 3
-		p todo = Todo.new(contents: todo_contents)
+		todo = Todo.new(contents: todo_contents)
 		# CSV.open("todos.csv", "a+") do |csv|
 		# 	csv << [todo_contents, todo.rating]
 		# end
-		p 'about to p todo'
-		p todo
-		$todos << todo
+		todo
+		$todos[todo.contents] = todo.rating
 	elsif option == 2
 		# start ranking todos
 		while true
 			p todos_to_compare = return_two_todos
 			compare(todos_to_compare[0], todos_to_compare[1])
-			break
+			# break
 		end
 	end
 	save_todos
 end
 
 def create_or_open_todos_file
-	$todos = []
+	$todos = {}
 	if File.exist?("todos.csv")
 		p "todos.csv already exists, so we'll be adding todos to that."
 		CSV.foreach("todos.csv") do |row|
-			$todos << Todo.new(contents: row[0], rating: row[1])
+			# $todos << Todo.new(contents: row[0], rating: row[1])
+			$todos[row[0]] = row[1]
 		end
 	else
 		todos_csv = CSV.open("todos.csv", "wb", headers: true, return_headers: false) do |csv|
